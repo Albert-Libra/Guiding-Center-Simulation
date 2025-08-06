8d451c25ca704ee812a5b859e99e95d503f216c5
Albert <luozhekai@pku.edu.cn>
2025-08-06 19:51:48 +0800

    first time to import igrf13 successfully!

--- guiding_center_solver/src/Solver.cpp
+++ guiding_center_solver/src/Solver.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
+extern "C" {
+    void igrf13syn_(int* isv, double* date, int* itype, double* alt, double* colat, double* elong, double* x, double* y, double* z, double* f);
+}
+
 int main() {
     
     cout << "Hello, World!" << endl;
     cout << "Hello, World!" << endl;
+
+    // Declare and initialize variables
+    int isv = 0;            // 0 for main field, 1 for secular variation
+    double date = 2020.0;   // Example date (year)
+    int itype = 1;          // 1 for geodetic, 2 for geocentric
+    double alt = 0.0;       // Altitude in km
+    double colat = 45.0;    // Colatitude in degrees (90 - latitude)
+    double elong = 120.0;   // East longitude in degrees
+    double x, y, z, f;      // Output variables
+
+    igrf13syn_(&isv, &date, &itype, &alt, &colat, &elong, &x, &y, &z, &f);
+
+    cout << "Magnetic field components: X=" << x << " Y=" << y << " Z=" << z << " F=" << f << endl;
 
     return 0;
 }
