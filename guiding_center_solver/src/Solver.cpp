5149e253026e9f18c30bf98537be19ba838686a8
Albert <luozhekai@pku.edu.cn>
2025-08-06 21:10:43 +0800

    change dll file path

--- guiding_center_solver/src/Solver.cpp
+++ guiding_center_solver/src/Solver.cpp
@@ -3,31 +3,6 @@
 
 using namespace std;
 
-// extern "C" {
-//     void igrf13syn_(int* isv, double* date, int* itype, double* alt, double* colat, double* elong, double* x, double* y, double* z, double* f); 
-// }
-
-// int main() {
-    
-//     cout << "Hello, World!" << endl;
-//     cout << "Hello, World!" << endl;
-
-//     // Declare and initialize variables
-//     int isv = 0;            // 0 for main field, 1 for secular variation
-//     double date = 2020.0;   // Example date (year)
-//     int itype = 1;          // 1 for geodetic, 2 for geocentric
-//     double alt = 0.0;       // Altitude in km
-//     double colat = 45.0;    // Colatitude in degrees (90 - latitude)
-//     double elong = 120.0;   // East longitude in degrees
-//     double x, y, z, f;      // Output variables
-
-//     igrf13syn_(&isv, &date, &itype, &alt, &colat, &elong, &x, &y, &z, &f);
-
-//     cout << "Magnetic field components: X=" << x << " Y=" << y << " Z=" << z << " F=" << f << endl;
-
-//     return 0;
-// }
-
 // 定义函数指针类型，参数和 Fortran 子程序一致
 typedef void (__stdcall *igrf13syn_func)(
     int*, double*, int*, double*, double*, double*, double*, double*, double*, double*
@@ -35,7 +10,7 @@ typedef void (__stdcall *igrf13syn_func)(
 
 int main() {
     // 加载 DLL（可指定绝对路径或相对路径）
-    HMODULE hDLL = LoadLibrary("..\\external\\IGRF\\igrf13.dll");
+    HMODULE hDLL = LoadLibraryW(L"..\\external\\IGRF\\*.dll");
     if (!hDLL) {
         cerr << "无法加载 DLL" << endl;
         return 1;
